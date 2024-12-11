8910337301d1ddf2bc219a73cd472886299790f8
simse <hello@simse.io>
2024-12-11 14:35:18 +0000

    feat: Add SplitSlice function

--- slices.go
+++ slices.go
@@ -1,15 +1,19 @@
 package batteries
 
-func UniqueElements[T comparable](input []T) []T {
-	var unique []T
-	m := map[T]bool{}
+// SplitSlice splits a slice evenly into N parts.
+// If the slice can't be split evenly, the last split will be short
+func SplitSlice[T any](input []T, splits int) [][]T {
+	// inputLen := len(input)
+	//danglingAmount := len(input) % splits
+	splitInput := make([][]T, splits)
 
-	for _, v := range input {
-		if !m[v] {
-			m[v] = true
-			unique = append(unique, v)
-		}
+	for i, val := range input {
+		// get split index
+		splitSubindex := i % splits
+		splitIndex := (i - splitSubindex) / splits
+
+		splitInput[splitIndex][splitSubindex] = val
 	}
 
-	return unique
+	return splitInput
 }
